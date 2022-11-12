e174c0a6109fc0d4613676e04e09b91540e276cc
Kit Yan <kityan3002@gmail.com>
2022-11-12 15:01:06 -0500

    layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 function App() {
   return (
     <>
+    <link rel="stylesheet" href="assets/css/style.css"></link>
       <input type = "text" />
       <h1>WASSUPPPP</h1>
       <button>Add ToDo</button>
