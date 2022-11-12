dc9c27d787d6f9f28e6e39683bd9c8cd1aa13d05
Fung-Sean <fsean100@gmail.com>
2022-11-12 17:04:36 -0500

    calendar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import './App.css';
+import 'react-calendar/dist/Calendar.css';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
   return (
-    <>
-      <input type = "text" />
-      <h1>WASSUPPPP</h1>
-      <button>Add ToDo</button>
-      <button>Clear ToDo</button>
-      <div>0 left</div>
-    </>
-  )
+    <div className='app'>
+      <h1 className='text-center'>React Calendar</h1>
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
+    </div>
+    
+  );
 }
 
 export default App;
