6a2ddabcdcc673890daa4d9e48704952d053a5e1
Fung-Sean <fsean100@gmail.com>
2022-11-12 13:50:49 -0500

    meow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input type = "text" />
+      <h1>WASSUPPPP</h1>
+      <button>Add ToDo</button>
+      <button>Clear ToDo</button>
+      <div>0 left</div>
+    </>
+  )
 }
 
 export default App;
