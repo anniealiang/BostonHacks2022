78807aa83dad78354c08da33f5f6fb6dfac5ea03
Kit Yan <kityan3002@gmail.com>
2022-11-12 20:13:58 -0500

    got rid of login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Calendar from 'react-calendar';
 import './App.css';
 import 'react-calendar/dist/Calendar.css';
 
+
 function App() {
     const [date, setDate] = useState(new Date());
   
@@ -12,11 +13,14 @@ function App() {
         <h1 className='text-center'>Mood Calendar</h1>
         <div className='calendar-container'>
           <Calendar onChange={setDate} value={date} />
-          <input type="text" placeholder="username" />
-          <input type="text" placeholder="password" />
-          <button>LOGIN</button>
-
+        </div> 
+        <div className='Login'>
+          <h1>Register</h1>
+          <input type="text" placeholder="username..." />
+          <input type="text" placeholder="password..." />
+          <button>Register</button>
         </div>
+          
         <p className='text-center'>
           <span className='bold'>Selected Date:</span>{' '}
           {date.toDateString()}
